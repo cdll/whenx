78e247b2f859c181470a7136019b85686ea35320
cdll <cd234350150@gmail.com>
2018-06-17 00:44:58 +0800

    upgrade whenx to 0.2.0 with whenx.uploadFile optimization;

--- src/thenx.js
+++ src/thenx.js
@@ -2,11 +2,18 @@
 const thenx= (name, opts= {})=>{
   // console.info(`func: ${name}`)
   return new Promise((resolve, reject)=>{
-    wx[name]( Object.assign(opts, {
+    const method= wx[name]( Object.assign(opts, {
       success: res=> resolve(res)
       ,fail: err=> reject(err)
       ,complete: opts.complete
     }) )
+    if(name== 'uploadFile'&& opts.progress){
+      let task= method
+      if(typeof opts.progress== "function"){
+        Function.call(opts.progress, task)
+      }
+      else console.warn(`[whenx]progress must be a function to call with task but not: ${typeof(opts.progress)}`)
+    }
   })
 }
 
--- whenx.js
+++ whenx.js
@@ -1,10 +1,10 @@
 /*
-	whenx v0.1.0
+	whenx v0.2.0
 	https://github.com/cdll/whenx
 	Released under the MIT License.
 */
 (function () {
-  var version = "0.1.0";
+  var version = "0.2.0";
 
   const whenx= function(){
     let _wx= {};
@@ -32,11 +32,18 @@
   const thenx= (name, opts= {})=>{
     // console.info(`func: ${name}`)
     return new Promise((resolve, reject)=>{
-      wx[name]( Object.assign(opts, {
+      const method= wx[name]( Object.assign(opts, {
         success: res=> resolve(res)
         ,fail: err=> reject(err)
         ,complete: opts.complete
       }) );
+      if(name== 'uploadFile'&& opts.progress){
+        let task= method;
+        if(typeof opts.progress== "function"){
+          Function.call(opts.progress, task);
+        }
+        else console.warn(`[whenx]progress must be a function to call with task but not: ${typeof(opts.progress)}`);
+      }
     })
   };
 
